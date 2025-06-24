734812627e589eb3f79772831d0873d75b6857e8
Nguyễn Vũ <nguyenvanvu112003@gmail.com>
2025-06-24 20:44:22 +0700

    đánh giá khóa học

--- src/routes/users_route.js
+++ src/routes/users_route.js
@@ -3,7 +3,7 @@ import user_controller from '../controllers/user_controller.js';
 const router = express.Router();
 
 // Lấy thông tin người dùng
-router.get('/infor/:userId', user_controller.getUserInfo);
+router.get("/:userId/get-user-info", user_controller.getUserInfo);
 // Cập nhật thông tin người dùng    
 router.put('/update/:userId', user_controller.updateUserInfo);
 
